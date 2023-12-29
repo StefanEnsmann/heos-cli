c47e4ae976b76a6f5c380f2be6066b5ffd4ab373
Stefan Ensmann <stefan@ensmann.de>
2023-12-29 12:47:34 +0100

    reduced recursion size

--- js/src/types/types.ts
+++ js/src/types/types.ts
@@ -4,10 +4,20 @@ export type QueueId = number;
 
 // recursive types bad, just for exercise
 type Length<T extends any[]> = T extends { length: infer L } ? L : never;
-type BuildTuple<L extends number, T extends any[] = []> = T extends { length: L } ? T : BuildTuple<L, [...T, any]>;
+type BuildTuple<L extends number, T extends any[] = []> = T extends {
+  length: L;
+}
+  ? T
+  : BuildTuple<L, [...T, any]>;
 type Inc<A extends number> = Length<[...BuildTuple<A>, any]>;
 type CommaSeparatedListSegment<T extends string | number> = `${T}`;
-export type CommaSeparatedList<T extends string | number, N = 0> = N extends 46 ? CommaSeparatedListSegment<T> : N extends number ? CommaSeparatedList<T, Inc<N>> | `${CommaSeparatedListSegment<T>},${CommaSeparatedList<T, Inc<N>>}` : never;
+export type CommaSeparatedList<T extends string | number, N = 0> = N extends 30
+  ? CommaSeparatedListSegment<T>
+  : N extends number
+  ?
+      | CommaSeparatedList<T, Inc<N>>
+      | `${CommaSeparatedListSegment<T>},${CommaSeparatedList<T, Inc<N>>}`
+  : never;
 
 export type QuickselectId = number;
 export type ContainerId = number;
